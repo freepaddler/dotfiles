15f0b437d1b788a4e6ab9e4ea8b67dc4b5b92f8f
Victor Chu <chu@vchu.net>
2024-03-21 20:14:11 +0800

    add brew-save
    vchu-macbookpro

---
+++ dot_local/bin/executable_brew-save.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+echo "Backing up homebrew..."
+echo
+
+DEST="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Sync/Homebrew"
+[ -d "$DEST" ] || mkdir -p "$DEST"
+mkdir -p "$DEST/archive"
+
+
+dmark=$(date +%Y-%m-%d_%H-%M-%S)
+[ -f "$DEST/Brewfile" ] && mv -fv "$DEST/Brewfile" "$DEST/archive/Brewfile_$dmark"
+
+HOMEBREW_NO_AUTO_UPDATE=1 brew bundle dump --file="$DEST/Brewfile"
+
+
+echo
+echo "Homebrew dump complete"
