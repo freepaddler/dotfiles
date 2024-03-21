c75cc10a8ccefa0975da59207870f600af2eb7a7
Victor Chu <chu@vchu.net>
2024-03-22 02:02:38 +0800

    vim: replace syntastic with ale
    vchu-macbookpro

--- dot_local/bin/executable_brew-save.sh
+++ dot_local/bin/executable_brew-save.sh
@@ -8,12 +8,11 @@ DEST="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Sync/Homebrew"
 [ -d "$DEST" ] || mkdir -p "$DEST"
 mkdir -p "$DEST/archive"
 
-
 dmark=$(date +%Y-%m-%d_%H-%M-%S)
 [ -f "$DEST/Brewfile" ] && mv -fv "$DEST/Brewfile" "$DEST/archive/Brewfile_$dmark"
 
 HOMEBREW_NO_AUTO_UPDATE=1 brew bundle dump --file="$DEST/Brewfile"
 
-
 echo
 echo "Homebrew dump complete"
+
