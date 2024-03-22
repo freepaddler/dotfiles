5cf2d595783ffd15a42283a71c180952574d09e2
Victor Chu <chu@vchu.net>
2024-03-23 04:15:20 +0800

    fix ish-update

--- ish-update.sh
+++ ish-update.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 # to be run from iOS ish emulator
-cp -rvf "$HOME"/dotfiles/dot_bash_completion.d "$HOME"/.bash_completion.d
+cp -rf "$HOME"/dotfiles/dot_bash_completion.d "$HOME"/.bash_completion.d
 mkdir -p "$HOME"/.vim
-cp -rvf "$HOME"/dotfiles/dot_vim/* "$HOME"/.vim/
+cp -rf "$HOME"/dotfiles/dot_vim/* "$HOME"/.vim/
 mkdir -p "$HOME"/.ssh/mastersocket
-cp -fv "$HOME"/dotfiles/dot_bash_functions "$HOME"/.bash_functions
-cp -fv "$HOME"/dotfiles/dot_bash_profile "$HOME"/.bash_profile
-cp -fv "$HOME"/dotfiles/dot_bashrc "$HOME"/.bashrc
-cp -fv "$HOME"/dotfiles/dot_inputrc "$HOME"/.inputrc
-cp -fv "$HOME"/dotfiles/dot_tmux.conf "$HOME"/.tmux.conf
-cp -fv "$HOME"/dotfiles/dot_vimrc "$HOME"/.vimrc
+cp -f "$HOME"/dotfiles/dot_bash_functions "$HOME"/.bash_functions
+cp -f "$HOME"/dotfiles/dot_bash_profile "$HOME"/.bash_profile
+cp -f "$HOME"/dotfiles/dot_bashrc "$HOME"/.bashrc
+cp -f "$HOME"/dotfiles/dot_inputrc "$HOME"/.inputrc
+cp -f "$HOME"/dotfiles/dot_tmux.conf "$HOME"/.tmux.conf
+cp -f "$HOME"/dotfiles/dot_vimrc "$HOME"/.vimrc
 age -d -i "$HOME"/.config/chezmoi.age "$HOME"/dotfiles/private_dot_ssh/encrypted_private_config.tmpl.age | sed "/^{{/d" >| "$HOME"/.ssh/config && chmod 600 "$HOME"/.ssh/config
 source "$HOME"/.bashrc
+echo
+echo "env updated"
