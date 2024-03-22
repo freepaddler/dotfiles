c378f069485dee58ba606e490ad0bd52d5274ecf
Victor Chu <chu@vchu.net>
2024-03-23 03:32:14 +0800

    fix ish-update

--- ish-update.sh
+++ ish-update.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 # to be run from iOS ish emulator
-PWD=$(pwd)
-
-cd $HOME
-rm -rf $HOME/dotfiles > /dev/null
-git clone --depth 1 https://github.com/freepaddler/dotfiles
-
 cp -rvf $HOME/dotfiles/dot_bash_completion.d $HOME/.bash_completion.d
 cp -rvf $HOME/dotfiles/dot_vim/* $HOME/.vim/
 mkdir -p $HOME/.ssh/mastersocket
@@ -17,9 +11,4 @@ cp -fv $HOME/dotfiles/dot_inputrc $HOME/.inputrc
 cp -fv $HOME/dotfiles/dot_tmux.conf $HOME/.tmux.conf
 cp -fv $HOME/dotfiles/dot_vimrc $HOME/.vimrc
 age -d -i $HOME/.config/chezmoi.age $HOME/dotfiles/private_dot_ssh/encrypted_private_config.tmpl.age | sed "/^{{/d" >| $HOME/.ssh/config && chmod 600 $HOME/.ssh/config
-
-mkdir -p $HOME/.local/bin
-cp -fv $HOME/dotfiles/dot_local/bin/ish-update.sh $HOME/.local/bin/
-rm -rf $HOME/dotfiles > /dev/null
-cd "$PWD"
 source $HOME/.bashrc
