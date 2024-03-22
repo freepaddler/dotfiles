bbaa48c1ea240dd62823280500d717ca478a80bf
Victor Chu <chu@vchu.net>
2024-03-23 00:16:00 +0800

    M  dot_local/bin/executable_brew-save.sh
    vchu-macbookpro

--- dot_local/bin/executable_brew-save.sh
+++ dot_local/bin/executable_brew-save.sh
@@ -4,12 +4,12 @@ set -e
 echo "Backing up homebrew..."
 echo
 
-DEST="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Sync/Homebrew"
+DEST="$HOME/Library/Mobile Documents/com~apple~CloudDocs/Sync"
 [ -d "$DEST" ] || mkdir -p "$DEST"
-mkdir -p "$DEST/archive"
+mkdir -p "$DEST/Homebrew.backup"
 
 dmark=$(date +%Y-%m-%d_%H-%M-%S)
-[ -f "$DEST/Brewfile" ] && mv -fv "$DEST/Brewfile" "$DEST/archive/Brewfile_$dmark"
+[ -f "$DEST/Brewfile" ] && mv -fv "$DEST/Brewfile" "$DEST/Homebrew.backup/Brewfile_$dmark"
 
 HOMEBREW_NO_AUTO_UPDATE=1 brew bundle dump --file="$DEST/Brewfile"
 
