e9980deca0044d4ac24316ddb7345c7bd880752a
Victor Chu <chu@vchu.net>
2024-08-18 00:21:16 +0800

    A  dot_local/bin/executable_certs-gen.sh
    vchu-macbookpro

---
+++ dot_local/bin/executable_certs-gen.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+DEST="./certs"
+#DEST=${1:-./certs}
+
+usage() {
+    cat <<EOF
+Generates test certificates: CA, intermediate CA, server and client.
+Both server and client certificates are signed by intermediate CA.
+
+Flags:
+    -d <destination directory>
+        default ./certs
+    -a <DNS:fqdn|IP:0.0.0.0>
+        default DNS:localhost,IP:127.0.0.1
+        key may be used multiple times
+EOF
+    exit 0
+}
+
+# adds Subject Alternative Names
+add_san() {
+    case "$1" in
+        DNS:*|IP:*)
+            if [ -z "$ALTNAMES" ]; then
+                ALTNAMES="$1"
+            else
+                ALTNAMES="$ALTNAMES,$1"
+            fi
+            ;;
+        *)
+            echo "use DNS:fqdn or IP:x.x.x.x as Subject Alternative Name" >&2
+            exit 1
+            ;;
+    esac
+}
+
+while getopts "d:a:h" OPT; do
+    case $OPT in
+        d) DEST=$OPTARG ;;
+        a) add_san "$OPTARG" ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
+
+[ -z "$ALTNAMES" ] && ALTNAMES=DNS:localhost,IP:127.0.0.1
+
+mkdir -p "$DEST" || { echo "Unable to create destination directory $DEST"; exit 1; }
+
+# we don't need libreSSL
+for e in $(which -a openssl); do
+    if "$e" version | grep -qi '^openssl'; then
+        alias openssl='$e'
+        break
+    fi
+done
+
+# root CA
+openssl req \
+    -subj "/CN=root CA" -out "$DEST"/ca.crt \
+    -newkey rsa:4096 -nodes -keyout "$DEST"/ca.key\
+    -x509 -days 365 \
+    -addext basicConstraints=critical,CA:TRUE \
+    -addext subjectKeyIdentifier=hash \
+    -addext authorityKeyIdentifier=keyid:always \
+    -addext keyUsage=critical,digitalSignature,cRLSign,keyCertSign
+
+# intermediate CA
+openssl req \
+    -subj "/CN=intermediate CA" -out "$DEST"/ca-int.crt \
+    -newkey rsa:4096 -nodes -keyout "$DEST"/ca-int.key \
+    -x509 -days 365 -CA "$DEST"/ca.crt -CAkey "$DEST"/ca.key \
+    -addext basicConstraints=critical,CA:TRUE,pathlen:0 \
+    -addext subjectKeyIdentifier=hash \
+    -addext authorityKeyIdentifier=keyid:always \
+    -addext keyUsage=critical,digitalSignature,cRLSign,keyCertSign
+
+# server certificate
+openssl req \
+    -subj "/CN=server certificate" -out "$DEST"/server.crt \
+    -newkey rsa:4096 -nodes -keyout "$DEST"/server.key \
+    -x509 -days 180 -CA "$DEST"/ca-int.crt -CAkey "$DEST"/ca-int.key \
+    -addext subjectAltName=$ALTNAMES \
+    -addext basicConstraints=critical,CA:FALSE \
+    -addext subjectKeyIdentifier=hash \
+    -addext authorityKeyIdentifier=keyid:always \
+    -addext keyUsage=critical,nonRepudiation,digitalSignature,keyEncipherment \
+    -addext extendedKeyUsage=serverAuth
+
+# client certificate
+openssl req \
+    -subj "/CN=client certificate" -out "$DEST"/client.crt \
+    -newkey rsa:4096 -nodes -keyout "$DEST"/client.key \
+    -x509 -days 180 -CA "$DEST"/ca-int.crt -CAkey "$DEST"/ca-int.key \
+    -addext basicConstraints=critical,CA:FALSE \
+    -addext subjectKeyIdentifier=hash \
+    -addext authorityKeyIdentifier=keyid:always \
+    -addext keyUsage=critical,nonRepudiation,digitalSignature,keyEncipherment \
+    -addext extendedKeyUsage=clientAuth,emailProtection
+
+# generate supplementary files
+cd "$DEST"
+cat server.crt ca-int.crt > server-fullchain.pem
+cat client.crt ca-int.crt > client-fullchain.pem
+cat ca.crt ca-int.crt > ca-all.pem
