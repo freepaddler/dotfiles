32df0b8360a2e208ad3c3812a77ad902b0bfc09b
Victor Chu <chu@vchu.net>
2024-08-21 01:46:19 +0800

    M  dot_local/bin/executable_certs-gen.sh
    vchu-macbookpro

--- dot_local/bin/executable_certs-gen.sh
+++ dot_local/bin/executable_certs-gen.sh
@@ -6,7 +6,7 @@ DEST="./certs"
 usage() {
     cat <<EOF
 Generates test certificates: CA, intermediate CA, server and client.
-Both server and client certificates are signed by intermediate CA.
+Both server and client certificates are signed by intermediate CA (if used).
 
 Flags:
     -d <destination directory>
@@ -14,6 +14,7 @@ Flags:
     -a <DNS:fqdn|IP:0.0.0.0>
         default DNS:localhost,IP:127.0.0.1
         key may be used multiple times
+    -i create intermediate CA
 EOF
     exit 0
 }
@@ -35,10 +36,11 @@ add_san() {
     esac
 }
 
-while getopts "d:a:h" OPT; do
+while getopts "d:a:ih" OPT; do
     case $OPT in
         d) DEST=$OPTARG ;;
         a) add_san "$OPTARG" ;;
+        i) ICA="true" ;;
         h) usage ;;
         *) usage ;;
     esac
@@ -66,21 +68,29 @@ openssl req \
     -addext authorityKeyIdentifier=keyid:always \
     -addext keyUsage=critical,digitalSignature,cRLSign,keyCertSign
 
+SIGN_CERT="$DEST"/ca.crt
+SIGN_KEY="$DEST"/ca.key
+
+if [ "$ICA" = "true" ]; then
 # intermediate CA
 openssl req \
     -subj "/CN=intermediate CA" -out "$DEST"/ca-int.crt \
     -newkey rsa:4096 -nodes -keyout "$DEST"/ca-int.key \
-    -x509 -days 365 -CA "$DEST"/ca.crt -CAkey "$DEST"/ca.key \
+    -x509 -days 365 -CA "$SIGN_CERT" -CAkey "$SIGN_KEY" \
     -addext basicConstraints=critical,CA:TRUE,pathlen:0 \
     -addext subjectKeyIdentifier=hash \
     -addext authorityKeyIdentifier=keyid:always \
     -addext keyUsage=critical,digitalSignature,cRLSign,keyCertSign
 
+SIGN_CERT="$DEST"/ca-int.crt
+SIGN_KEY="$DEST"/ca-int.key
+fi
+
 # server certificate
 openssl req \
     -subj "/CN=server certificate" -out "$DEST"/server.crt \
     -newkey rsa:4096 -nodes -keyout "$DEST"/server.key \
-    -x509 -days 180 -CA "$DEST"/ca-int.crt -CAkey "$DEST"/ca-int.key \
+    -x509 -days 180 -CA "$SIGN_CERT" -CAkey "$SIGN_KEY" \
     -addext subjectAltName=$ALTNAMES \
     -addext basicConstraints=critical,CA:FALSE \
     -addext subjectKeyIdentifier=hash \
@@ -92,7 +102,7 @@ openssl req \
 openssl req \
     -subj "/CN=client certificate" -out "$DEST"/client.crt \
     -newkey rsa:4096 -nodes -keyout "$DEST"/client.key \
-    -x509 -days 180 -CA "$DEST"/ca-int.crt -CAkey "$DEST"/ca-int.key \
+    -x509 -days 180 --CA "$SIGN_CERT" -CAkey "$SIGN_KEY" \
     -addext basicConstraints=critical,CA:FALSE \
     -addext subjectKeyIdentifier=hash \
     -addext authorityKeyIdentifier=keyid:always \
@@ -100,7 +110,9 @@ openssl req \
     -addext extendedKeyUsage=clientAuth,emailProtection
 
 # generate supplementary files
+if [ "$ICA" = "true" ]; then
 cd "$DEST"
 cat server.crt ca-int.crt > server-fullchain.pem
 cat client.crt ca-int.crt > client-fullchain.pem
 cat ca.crt ca-int.crt > ca-all.pem
+fi
