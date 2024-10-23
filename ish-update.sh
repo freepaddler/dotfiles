6b2ce2e7b53240389a82131e8ac53896056ccd83
Victor Chu <chu@vchu.net>
2024-10-23 21:47:22 +0800

    M  dot_inputrc M  dot_tmux.conf R  dot_vim/colors/desert.vim -> dot_vim/colors/desertold.vim M  dot_vimrc M  ish-update.sh M  private_Library/private_Preferences/private_com.apple.Terminal.plist
    vchu-macbookpro

--- ish-update.sh
+++ ish-update.sh
@@ -2,15 +2,17 @@
 set -e
 # to be run from iOS ish emulator
 cp -rf "$HOME"/dotfiles/dot_bash_completion.d "$HOME"/.bash_completion.d
-mkdir -p "$HOME"/.vim
-cp -rf "$HOME"/dotfiles/dot_vim/* "$HOME"/.vim/
-mkdir -p "$HOME"/.ssh/mastersocket
-cp -f "$HOME"/dotfiles/dot_bash_functions "$HOME"/.bash_functions
+cp -rf "$HOME"/dotfiles/dot_bashrc.d "$HOME"/.bashrc.d
 cp -f "$HOME"/dotfiles/dot_bash_profile "$HOME"/.bash_profile
 cp -f "$HOME"/dotfiles/dot_bashrc "$HOME"/.bashrc
 cp -f "$HOME"/dotfiles/dot_inputrc "$HOME"/.inputrc
 cp -f "$HOME"/dotfiles/dot_tmux.conf "$HOME"/.tmux.conf
+
+mkdir -p "$HOME"/.vim
+cp -rf "$HOME"/dotfiles/dot_vim/* "$HOME"/.vim/
 cp -f "$HOME"/dotfiles/dot_vimrc "$HOME"/.vimrc
+
+mkdir -p "$HOME"/.ssh/mastersocket
 age -d -i "$HOME"/.config/chezmoi.age "$HOME"/dotfiles/private_dot_ssh/encrypted_private_config.tmpl.age | sed "/^{{/d" >| "$HOME"/.ssh/config && chmod 600 "$HOME"/.ssh/config
 source "$HOME"/.bashrc
 echo
