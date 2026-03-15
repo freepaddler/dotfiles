91b6a0c2956be19e191d882ff4fd43e54f0f94ac
Victor Chu <chu@vchu.net>
2026-03-15 22:15:47 +0200

    remove obsolete migration scripts

--- .chezmoiscripts/run_before_move-age-key.sh
+++ .chezmoiscripts/run_before_move-age-key.sh
@@ -4,7 +4,9 @@ chezmoiDir="$HOME/.config/chezmoi"
 mkdir -p "$chezmoiDir"
 
 for dir in "$HOME" "$HOME/.config"; do
-    [ -f "$dir/chezmoi.age" ] && mv -f "$dir/chezmoi.age" "$chezmoiDir/"
+    [ -f "$dir/chezmoi.age" ] || continue
+    echo "moving $dir/chezmoi.age to $chezmoiDir/"
+    mv -f "$dir/chezmoi.age" "$chezmoiDir/"
 done
 
 exit 0
