9da7e72de55e8f67c3499a103da11bad28e24b00
Victor Chu <chu@vchu.net>
2026-08-21 00:21:34 +0300

    save
    archui

--- private_dot_local/bin/scripts/executable_check_rclone_icloud_auth.sh
+++ private_dot_local/bin/scripts/executable_check_rclone_icloud_auth.sh
@@ -5,7 +5,7 @@ if rclone lsd icloud: > /dev/null 2>&1; then
     text='👌 iCloud connection ok'
 else
     icon='🟥'
-    text=$(printf '%s\n%s\n%s' "⚠️ iCloud connection failed! Run:" "rclone-icloud config reconnect icloud:" "systemctl --user restart app-icloud@autostart.service")
+    text=$(printf '%s\n%s\n%s\n%s' "⚠️ iCloud connection failed! Run:" "rclone-icloud config reconnect icloud:" "systemctl --user restart app-mount\\x2dicloud@autostart.service" "systemctl --user restart app-mount\\x2dicloud\\x2dphotos@autostart.service")
 fi
 
 case $1 in
